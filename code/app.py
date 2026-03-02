0261429765bcf1e9984742897736eb304d8add34
sabtriestech <sbeaver@uchicago.edu>
2026-03-02 13:40:11 -0600

    problem solving continued

--- code/app.py
+++ code/app.py
@@ -19,31 +19,6 @@ alt.data_transformers.disable_max_rows()
 
 BASE_DIR = Path(__file__).parent.parent
 
-# improve graph resolution
-#import tempfile
-#from IPython.display import SVG, display, Image
-#import vl_convert as vlc
-
-#def display_altair_png(chart, scale=2):
-    """
-    Render an Altair chart to a PNG and display it inline.
-
-    Parameters
-    ----------
-    chart : altair.Chart
-        Altair chart object to render.
-    scale : int, optional
-        Resolution scaling factor for the PNG (default = 2). Use scale=2 for standard slides. Use scale = 3–4 for dense figures or PDF exports.
-    """
-    #png_bytes = vlc.vegalite_to_png(chart.to_dict(), scale=scale)
-
-    # Write to a temporary PNG file and display
-    #with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
-     #   tmp.write(png_bytes)
-     #   tmp.flush()
-     #   display(Image(filename=tmp.name))
-
-
 st.set_page_config(page_title='Economic Mobility, Wages, and Crime')
 st.title('Economic Mobility, Wages, and Crime')
 
